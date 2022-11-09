658f27e8dfd95cd280e23acee95b76a29a979a6f
dvitel <viteldmitry@gmail.com>
2022-11-09 12:41:01 -0500

    fix of path

--- g3.sh
+++ g3.sh
@@ -8,4 +8,4 @@
 #SBATCH --gpus=1 # 1 GPU
 #SBATCH --exclude GPU41
 conda activate semParse2
-srun /home/d/dvitel/semp/start.sh g3
+srun /home/d/dvitel/semp/start.sh '/home/d/dvitel/semp/g3'
--- g4.sh
+++ g4.sh
@@ -8,4 +8,4 @@
 #SBATCH --gpus=1 # 1 GPU
 #SBATCH --exclude GPU41
 conda activate semParse2
-srun /home/d/dvitel/semp/start.sh g4
+srun /home/d/dvitel/semp/start.sh '/home/d/dvitel/semp/g4'
