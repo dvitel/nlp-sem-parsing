2eb5216ce84ca918cb2b12739fcde0365cbf52e0
dvitel <viteldmitry@gmail.com>
2022-11-12 17:41:55 -0500

    change out folder

--- h4.sh
+++ h4.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 #All options below are recommended
-#SBATCH --job-name=semp-h3
-#SBATCH -o h3.out
-#SBATCH -e h3.err
+#SBATCH --job-name=semp-h4
+#SBATCH -o h4.out
+#SBATCH -e h4.err
 #SBATCH -D /data/dvitel/semParse
 #SBATCH -p Quick # run on partition general
 #SBATCH --gpus=1 # 1 GPU
 #SBATCH --exclude GPU41
 conda activate semParse2
-srun /home/d/dvitel/semp/start.sh '/home/d/dvitel/semp/h3'
+srun /home/d/dvitel/semp/start.sh '/home/d/dvitel/semp/h4'
