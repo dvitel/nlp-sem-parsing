703e1333bf213c6391046924f9ef218453b72678
dvitel <viteldmitry@gmail.com>
2023-01-21 16:20:20 -0500

    fix of ge sbatch file

--- ge.sh
+++ ge.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -l
 #All options below are recommended
-#SBATCH --job-name=ge-${1//\./_}
-#SBATCH -o ge-${1//\./_}.out
-#SBATCH -e ge-${1//\./_}.err
 #SBATCH -D /data/dvitel/semParse
 #SBATCH -p Quick # run on partition general
 #SBATCH --gpus=1 # 1 GPU
 #SBATCH -w GPU45
 conda activate semParse2
-srun python3 /home/d/dvitel/semp/ge.py $1 $2
+srun -J "ge-${1//\./_}" -o "ge-${1//\./_}.out" -e "ge-${1//\./_}.err" python3 /home/d/dvitel/semp/ge.py $1 $2
